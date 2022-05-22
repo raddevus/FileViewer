1790a03a7c88e4e7706280e091cbbfece1c1ca12
raddevus <1radeutsch1@gmail.com>
2022-05-22 18:13:38 -0400

    Renamed unit test to proper name. Removed unused fake test & added two valid tests.

---
+++ FileViewer.Tests/FileSystemTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System.Collections.Generic;
+
+namespace FileViewer.Tests
+{
+    public class FileSystemTests
+    {
+        [Fact]
+        public void GetFileSystemItems(){
+            FileSystem fs = new FileSystem(@"c:\users\");
+            List<FileSystemItem> allItems = fs.GetAllFileSystemItems();
+            Assert.True(allItems.Count > 0);
+
+        }
+
+        [Fact]
+        public void GetFileSystemErrors(){
+            FileSystem fs = new FileSystem(@"c:\invalidpath");
+            Assert.Throws<System.IO.DirectoryNotFoundException>(() => fs.GetAllFileSystemItems());
+        }
+    }
+
+    
+}
