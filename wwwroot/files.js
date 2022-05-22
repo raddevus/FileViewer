a2aded2c487df896b1506a8463e215cc5e779c3b
raddevus <1radeutsch1@gmail.com>
2022-05-22 12:47:11 -0400

    Added new td for icon (first item in row) & added table-hover style so user can more easily tell which row the cursor is on.

--- wwwroot/files.js
+++ wwwroot/files.js
@@ -54,7 +54,8 @@ function FileSystemTable(fileSystemInfo){
           React.createElement("td",{width:"150px"}, 
           React.createElement("svg", isDirectory(fileSystemInfo[x].Type) ? folderIcon[0]: fileIcon[0],
             React.createElement("path", isDirectory(fileSystemInfo[x].Type) ? folderIcon[1]: fileIcon[1] ,null)
-          ), fileSystemInfo[x].Name),
+          ), ),
+          React.createElement("td",null,fileSystemInfo[x].Name),
           React.createElement("td",null, fileSystemInfo[x].Type),
           React.createElement("td",null, fileSystemInfo[x].FullName)
           )
